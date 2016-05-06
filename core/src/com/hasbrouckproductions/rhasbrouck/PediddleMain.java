e8fadbe6c9bbdcee215d663d478660bb6610ba71
hasbrour1-wk <roberthasbrouck@gmail.com>
2016-05-06 14:34:43 -0400

    Added cars to popup randomly on each side

--- core/src/com/hasbrouckproductions/rhasbrouck/PediddleMain.java
+++ core/src/com/hasbrouckproductions/rhasbrouck/PediddleMain.java
@@ -18,14 +18,21 @@ import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.TimeUtils;
 
+import org.w3c.dom.css.Rect;
+
 public class PediddleMain extends ApplicationAdapter {
 
 	//Asset import
 	private Texture audiImage;
 	private Texture roadImage;
 	private Texture customRoad;
+	private Texture sportCar;
+	private Texture taxiImage;
+
 	private Sound carSound;
 	private Sprite roadSprite;
+	private Sprite sportSprite;
+	private Sprite taxiSprite;
 
 	//Camera and Sprite Batch
 	private SpriteBatch batch;
@@ -35,7 +42,12 @@ public class PediddleMain extends ApplicationAdapter {
 
 	private Array<Rectangle> roads;
 	private Array<Rectangle> roads2;
+	private Array<Rectangle> cars;
+	private Array<Rectangle> taxis;
+
 	private long lastRoadTime;
+	private long lastCarTime;
+	private long lastTaxiTime;
 
 
 	@Override
@@ -44,6 +56,18 @@ public class PediddleMain extends ApplicationAdapter {
 		audiImage = new Texture(Gdx.files.internal("Audi.png"));
 		roadImage = new Texture(Gdx.files.internal("roadmarker.png"));
 		customRoad = new Texture(Gdx.files.internal("roadcustom.png"));
+		sportCar = new Texture(Gdx.files.internal("Car.png"));
+		taxiImage = new Texture(Gdx.files.internal("taxi.png"));
+
+		sportSprite = new Sprite(sportCar);
+		sportSprite.setSize(120, 120);
+		sportSprite.setOrigin(0,0);
+		sportSprite.setRotation(-180);
+
+		taxiSprite = new Sprite(taxiImage);
+		taxiSprite.setSize(120, 120);
+
+
 		roadSprite = new Sprite(roadImage);
 
 		//load car sound
@@ -62,12 +86,16 @@ public class PediddleMain extends ApplicationAdapter {
 		mainCar = new Rectangle();
 		mainCar.x = 800/2 - 64 / 2;
 		mainCar.y = 40;
-		mainCar.width = 120;
+		mainCar.width = 60;
 		mainCar.height = 120;
 
 		roads = new Array<Rectangle>();
 		roads2 = new Array<Rectangle>();
+		cars = new Array<Rectangle>();
+		taxis = new Array<Rectangle>();
 		spawnRoad();
+		spawnCar();
+		spawnTaxi();
 	}
 
 	@Override
@@ -84,6 +112,8 @@ public class PediddleMain extends ApplicationAdapter {
 		//start rendering opjects
 		batch.begin();
 
+
+
 		batch.draw(customRoad, 0, 0);
 		for(Rectangle road: roads){
 			roadSprite.setX(road.x);
@@ -95,6 +125,17 @@ public class PediddleMain extends ApplicationAdapter {
 			roadSprite.setY(road.y);
 			roadSprite.draw(batch);
 		}
+		for(Rectangle car: cars){
+			sportSprite.setX(car.x);
+			sportSprite.setY(car.y);
+			sportSprite.draw(batch);
+		}
+		for(Rectangle taxi: taxis){
+			taxiSprite.setX(taxi.x);
+			taxiSprite.setY(taxi.y);
+			taxiSprite.draw(batch);
+		}
+
 		batch.draw(audiImage, mainCar.x, mainCar.y, 120, 120);
 
 		batch.end();
@@ -115,6 +156,10 @@ public class PediddleMain extends ApplicationAdapter {
 		//road spawn
 		if(TimeUtils.nanoTime() - lastRoadTime > 1000000000 / 2) spawnRoad();
 
+		if(TimeUtils.nanoTime() - lastCarTime > 1000000000 * 2) spawnCar();
+
+		if(TimeUtils.nanoTime() - lastTaxiTime > 1000000000 * 1.5) spawnTaxi();
+
 		Iterator<Rectangle> iter = roads.iterator();
 		while(iter.hasNext()){
 			Rectangle road = iter.next();
@@ -132,6 +177,24 @@ public class PediddleMain extends ApplicationAdapter {
 				iter2.remove();
 			}
 		}
+
+		Iterator<Rectangle> iter3 = cars.iterator();
+		while(iter3.hasNext()){
+			Rectangle car = iter3.next();
+			car.y -= 300 *(Gdx.graphics.getDeltaTime());
+			if(car.y + 200 < 0)iter3.remove();
+		}
+
+		Iterator<Rectangle> iter4 = taxis.iterator();
+		while(iter4.hasNext()){
+			Rectangle taxi = iter4.next();
+			taxi.y -= 250 * (Gdx.graphics.getDeltaTime());
+			if(taxi.y + 200 < 0)iter4.remove();
+			if(taxi.overlaps(mainCar)){
+				//crash
+				iter4.remove();
+			}
+		}
 	}
 
 	public void spawnRoad(){
@@ -143,6 +206,38 @@ public class PediddleMain extends ApplicationAdapter {
 		roads.add(road);
 		roads2.add(road);
 		lastRoadTime = TimeUtils.nanoTime();
+	}
+
+	public void spawnCar(){
+		Rectangle car = new Rectangle();
+		//random side of road
+		//220 for left 350 for right
+		if(Math.random() >= 0.5){
+			car.x = 220;
+		}else{
+			car.x = 350;
+		}
+		car.y = 600;
+		car.width = 120;
+		car.height = 120;
+		cars.add(car);
+		lastCarTime = TimeUtils.nanoTime();
+	}
+
+	public void spawnTaxi(){
+		Rectangle taxi = new Rectangle();
+		//random side of road
+		//right side 600 left 480
+		if(Math.random() >= 0.5){
+			taxi.x = 600;
+		}else{
+			taxi.x = 480;
+		}
+		taxi.y = 600;
+		taxi.width = 120;
+		taxi.height = 60;
+		taxis.add(taxi);
+		lastTaxiTime = TimeUtils.nanoTime();
 	}
 
 	@Override
